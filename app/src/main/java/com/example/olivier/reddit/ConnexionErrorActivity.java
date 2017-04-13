16778d48d64f187c8fcf7a5013d39f0095037066
Judelin <seide_judelin@yahoo.fr>
2017-04-13 14:04:59 -0400

    premier commit

--- app/src/main/java/com/example/olivier/reddit/ConnexionErrorActivity.java
+++ app/src/main/java/com/example/olivier/reddit/ConnexionErrorActivity.java
@@ -1,14 +1,13 @@
 package com.example.olivier.reddit;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class ConnexionErrorActivity extends AppCompatActivity implements View.OnClickListener {
-    Button connexionButton;
+    Button connexionButton;//bouton de connection
     String sectionsel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
